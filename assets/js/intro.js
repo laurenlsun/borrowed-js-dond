2fb56f88b72f0e113439470908680c3b3a5141c7
Lauren Sun <119720461+laurenlsun@users.noreply.github.com>
2023-07-26 23:56:00 -0400

    Add files via upload

---
+++ assets/js/intro.js
@@ -0,0 +1,85 @@
+//Music API(RapidAPI-Deezer) for Intro
+//Global Variables
+var playlist = [];
+var searchMusic;
+var sound = {};
+let isPlaying = false;
+//Ajax call function
+function ajaxCall(searchMusic) {
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + searchMusic,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      "x-rapidapi-key": "b1b91f9733mshf756f13729cdbcep17742fjsn1c8927118912",
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    playlist = [];
+    for (i = 0; i < response.data.length; i++) {
+      playlist.push(response.data[i].preview);
+    }
+    playMusic(0, playlist);
+  });
+}
+
+//Function to play music (Howler Js)
+function playMusic(i, playlist) {
+  if (isPlaying) {
+    sound.stop();
+  }
+  sound = new Howl({
+    src: [playlist[i]],
+    onend: function () {
+      if (i + 1 == playlist.length) {
+        playMusic(0, playlist);
+      } else {
+        playMusic(i + 1, playlist);
+      }
+    },
+  });
+  isPlaying = true;
+  sound.play();
+  //Resume, Pause, & Stop button functions
+  $("#resumeBtn").on("click", function () {
+    sound.mute(false);
+  });
+  $("#pauseBtn").on("click", function () {
+    sound.mute(true);
+  });
+  $("#stopBtn").on("click", function () {
+    sound.stop();
+  });
+}
+//On Click for our search Btn
+var soundIsPlaying = false;
+$("#add-music").on("click", function () {
+  event.preventDefault();
+  playlist = [];
+  searchMusic = $("#music-input").val().trim();
+  ajaxCall(searchMusic);
+});
+
+
+//function i added :)
+
+function save_game(data) {
+  fetch('http://testdond.pythonanywhere.com/save_player', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Process the response data (if needed)
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
