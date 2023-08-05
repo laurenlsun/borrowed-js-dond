5b60673021517f2d068bf8d33343829e1ce6604f
Lauren Sun <119720461+laurenlsun@users.noreply.github.com>
2023-08-04 21:56:25 -0400

    Update welcome.js

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,11 +1,14 @@
 document.getElementById("continue-btn").addEventListener("click", function() {
     // Generate a random number between 0 and 1
     var randomNumber = Math.random();
-  
+
     // 50% chance to redirect to google and 50% chance to redirect to usc.edu
     if (randomNumber < 0.5) {
       window.location.href = "b-intro.html";
+      localStorage.setItem("first_mode", "b");
     } else {
       window.location.href = "p-intro.html";
+      localStorage.setItem("first_mode", "p");
     }
+    console.log("first_mode:" + localStorage.getItem(first_mode));
 });
